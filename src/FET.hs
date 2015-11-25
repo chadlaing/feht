e39fc5a761c87cf508108a27e5fcaafd6036190a
Chad Laing <chadlaing@gmail.com>
2015-11-25 09:30:46 -0700

    First commit with stack

---
+++ src/FET.hs
@@ -0,0 +1,96 @@
+module Hstats.FET where
+
+import qualified Data.ByteString.Lazy.Char8             as BS
+import           Data.Eq
+import           Data.Function
+import           Data.Functor
+import           Data.Int
+import           Data.List
+import           Data.Ord
+import           Prelude                                (Double, fromIntegral,
+                                                         (+))
+import           Statistics.Distribution                (cumulative,
+                                                         probability)
+import           Statistics.Distribution.Hypergeometric (hypergeometric)
+import           Text.Show
+
+--the Fisher's exact test can be computed from the hypergeometric distribution
+--eg. test whether the presence / absence of a gene is statistically significant
+--between two bacterial groups
+--           Group 1 | Group 2
+--          -------------------
+-- has gene |   2    |    7
+-- no gene  |   5    |    3
+--          -------------------
+-- the hypergeometric distribution is calculated in Statistics.Distribution.Hypergeometric
+-- hypergeometric = m l k
+-- From the package description: The parameters of the distribution describe
+-- k elements chosen from a population of l, with m elements of one type,
+-- and l-m of the other (all are positive integers).
+-- In our case:
+-- m is the sum of "has gene" instances, so 9
+-- l is the total population size, so 17
+-- k is the sum of a particular serotype, so 7
+-- let d = distribution 9 17 7
+-- This gives us the distribution, but we need to test the probability
+-- that the presence of this gene is statistically different, using the two-tail
+-- approach.
+-- The one-tail probability in our example of Group 1 having 2 members with the
+-- gene can be tested using the cumulative distribution function from
+-- Statistics.Distribution, our previously calculated distribution "d", and
+-- the number of "successes" (x), which is 2.
+-- let oneTail = cumulative d 2 = 0.11703002879473468
+-- However, we have no previous expectation for this group to have or not have
+-- this gene, so what we really want is the two-tailed test.
+-- Please see http://www.pmean.com/11/Fishers.html for an excellent,
+-- detailed description.
+-- In short, we need to sum all of the probabilities that are smaller than or
+-- equal to the probability of the observed table (eg. x = 2 in our example).
+-- The twoTailValue is calculated by generating all of the probabilities for
+-- our sample, filtering the ones that have a probability too large, and
+-- summing the remaining values.
+
+newtype GroupOneA = GroupOneA { unGroupOneA :: Int } deriving (Eq, Show)
+newtype GroupOneB = GroupOneB { unGroupOneB :: Int } deriving (Eq, Show)
+newtype GroupTwoA = GroupTwoA { unGroupTwoA :: Int } deriving (Eq, Show)
+newtype GroupTwoB = GroupTwoB { unGroupTwoB :: Int } deriving (Eq, Show)
+newtype PValue = PValue {unPValue :: Double} deriving (Eq, Show)
+newtype FETName = FETName {unFETName :: BS.ByteString} deriving(Eq, Show)
+data FETMode = OneTail | TwoTail deriving (Eq,Show)
+
+data FETResult = FETResult{groupOneA :: Int
+                          ,groupOneB :: Int
+                          ,groupTwoA :: Int
+                          ,groupTwoB :: Int
+                          ,pvalue    :: Double
+                          ,fetName   :: BS.ByteString} deriving (Eq, Show)
+
+fet :: FETName
+    -> GroupOneA
+    -> GroupOneB
+    -> GroupTwoA
+    -> GroupTwoB
+    -> FETMode
+    -> FETResult
+fet (FETName fName) (GroupOneA goa) (GroupOneB gob) (GroupTwoA gta) (GroupTwoB gtb) mode
+  = case mode of
+        OneTail -> theResult{pvalue = cumD}
+        TwoTail -> theResult{pvalue = twoTailValue}
+  where
+    cumD = cumulative d $ fromIntegral goa
+    probX = probability d goa
+    twoTailValue = sum . filter (<= probX) . fmap (probability d) $ [0..k]
+    d = hypergeometric m l k
+    m = goa + gta
+    l = m + gob + gtb
+    k = goa + gob
+    theResult = FETResult{groupOneA = goa
+                         ,groupOneB = gob
+                         ,groupTwoA = gta
+                         ,groupTwoB = gtb
+                         ,pvalue = 1
+                         ,fetName = fName
+                         }
+
+
+
---
+++ src/Table.hs
@@ -0,0 +1,254 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+
+module Hstats.Table where
+
+import           Data.Bool
+import qualified Data.ByteString.Lazy.Char8 as BS
+import           Data.Char
+import           Data.Eq
+import           Data.Foldable
+import           Data.Function
+import           Data.Functor               (fmap)
+import           Data.Hashable
+import qualified Data.HashMap.Strict        as M
+import           Data.Int
+import           Data.List                  (zip, (++))
+import           Data.Maybe
+import           Data.Ord
+import           Data.String
+import qualified Data.Time.Calendar         as C
+import qualified Data.Vector.Unboxed        as V
+import           GHC.Generics               (Generic)
+import           Prelude                    (Integer, error, (+))
+import           Text.Read
+import           Text.Show
+
+
+newtype GenomeName = GenomeName {unGenomeName :: BS.ByteString } deriving (Show, Eq, Ord, Generic)
+-- newtype Serotype = Serotype { unSerotype :: BS.ByteString } deriving (Show, Eq, Ord, Generic)
+-- newtype Source = Source { unSource :: BS.ByteString } deriving (Show, Eq, Ord, Generic)
+-- newtype Country = Country { unCountry :: BS.ByteString } deriving (Show, Eq, Ord, Generic)
+-- newtype Province = Province { unProvince :: BS.ByteString } deriving (Show, Eq, Ord, Generic)
+-- newtype IsolationDate = IsolationDate { unIsolationDate :: Maybe C.Day } deriving (Show, Eq, Ord, Generic)
+--newtype ColumnNumber = ColumnNumber { unColumnNumber :: Maybe Int} deriving (Show, Eq, Ord)
+newtype GeneName = GeneName { unGeneName :: BS.ByteString } deriving (Eq, Show, Ord, Generic)
+
+--make classes instances of Hashable
+--this requires the import of GHC.Generics (Generic) and Data.Hashable
+--also requires the Type to derive Generic
+--then a new instance for Hashable can be created
+instance Hashable GeneName
+instance Hashable GenomeName
+instance Hashable Metadata
+
+
+
+data Metadata = Serotype {unSerotype :: BS.ByteString }
+              | Source { unSource :: BS.ByteString }
+              | Country { unCountry :: BS.ByteString }
+              | Province { unProvince :: BS.ByteString }
+              | ColumnNumber {unColumnNumber :: Maybe Int}
+              deriving (Eq, Show, Ord, Generic)
+
+
+data Table = Table{serotype     :: Metadata
+                  ,source       :: Metadata
+                  ,country      :: Metadata
+                  ,province     :: Metadata
+                  ,columnNumber :: Metadata } deriving (Eq, Show, Ord, Generic)
+
+
+fromMetadata :: Metadata -> BS.ByteString
+fromMetadata x = case x of
+  Serotype _-> unSerotype x
+  Source _-> unSource x
+  Country _-> unCountry x
+  Province _-> unProvince x
+  ColumnNumber _ ->
+
+
+defaultTable :: Table
+defaultTable = Table
+    {serotype = Serotype defaultNa
+    ,source = Source defaultNa
+    ,country = Country defaultNa
+    ,province = Province defaultNa
+    ,columnNumber = ColumnNumber Nothing}
+
+
+getMetadataType :: Metadata -> Table -> Metadata
+getMetadataType x = case x of
+  Serotype _ -> serotype
+  Source _ -> source
+  Country _ -> country
+  Province _ -> province
+  ColumnNumber _ -> columnNumber
+
+
+defaultNa :: BS.ByteString
+defaultNa = BS.pack "NA"
+
+
+
+getListOfMetadata :: [(Int,String)] -> [Metadata]
+getListOfMetadata = foldl' insertMetadata []
+  where
+    insertMetadata :: [Metadata]
+                   -> (Int, String)
+                   -> [Metadata]
+    insertMetadata xs (i,s) = let mList = fmap BS.pack . words $ s in
+      case i of
+        1 -> fmap Serotype mList ++ xs
+        2 -> fmap Source mList ++ xs
+        3 -> fmap Country mList ++ xs
+        4 -> fmap Province mList ++ xs
+        _ -> error "Unknown metadata type"
+
+
+
+--For each line in the file, except the first one, we want to create a record
+--with all of the information given in Table
+--the first one contains column headers
+getMetadataFromFile :: [[BS.ByteString]] -> M.HashMap GenomeName Table
+getMetadataFromFile [] = error "File is empty"
+getMetadataFromFile (_:xs) = foldl' getEntry M.empty xs
+
+
+getEntry :: M.HashMap GenomeName Table
+         -> [BS.ByteString]
+         -> M.HashMap GenomeName Table
+getEntry hm [] = hm
+getEntry hm (name:xs) = M.insert (GenomeName name) newMeta hm
+  where
+    newMeta = foldl' getTableValues defaultTable $ zip xs [1..]
+
+
+--date comes from CSV where in format 2000/01/07
+getTableValues :: Table -> (BS.ByteString, Int) -> Table
+getTableValues t (x,i)
+    | i == 1 = t{serotype = Serotype x}
+    | i == 2 = t{source = Source x}
+    | i == 3 = t{country = Country x}
+    | i == 4 = t{province = Province x}
+    | otherwise = t
+
+
+
+--we don't want to have to pass default values to the
+--public facing functions. Partially apply this
+--function to the "workhorse" private function
+--that automatically supplies the defaults
+--eg. first column contains row names, so skip
+addColumnNumbers :: M.HashMap GenomeName Table
+                 -> [BS.ByteString]
+                 -> M.HashMap GenomeName Table
+addColumnNumbers _ [] = error "No data sent to addColumnNumbers"
+addColumnNumbers hm (_:xs) = ggn $ zip xs [0..]
+  where
+    ggn :: [(BS.ByteString, Int)] -> M.HashMap GenomeName Table
+    ggn = foldl' addColumnToTable hm
+    --we will use the adjust function
+    --only changes a value if it exists
+    --otherwise, returns the original HashMap
+    addColumnToTable :: M.HashMap GenomeName Table
+                     -> (BS.ByteString, Int)
+                     -> M.HashMap GenomeName Table
+    addColumnToTable hm' (x,i) = M.adjust addColumn (GenomeName x) hm'
+      where
+        addColumn :: Table -> Table
+        addColumn t = t{columnNumber = ColumnNumber $ Just i}
+
+
+
+getDateFromCSV :: BS.ByteString -> Maybe C.Day
+getDateFromCSV x =  case BS.split '/' x of
+    [year,month,day] -> Just $ C.fromGregorian (read (BS.unpack year)::Integer)
+                          (intValue month) (intValue day)
+    _ -> Nothing
+
+
+intValue :: BS.ByteString -> Int
+intValue x = read (BS.unpack x)::Int
+
+
+--this assumes we have stripped the header line and have [[genename:values]]
+--the char is the delimiter used for splitting each data line
+getGeneVectorMap :: Char
+                 -> [BS.ByteString]
+                 -> M.HashMap GeneName (V.Vector Char)
+getGeneVectorMap delimiter = foldl' addGeneData M.empty
+  where
+    --each line of the input file is delimited
+    --the gene name is first, followed by the single-char values for each genome
+    --we break each line into gene name and data with break
+    --the vData that remains has the delimiters that need to be eliminated
+    --and the ByteString needs to become a Vector
+    addGeneData :: M.HashMap GeneName (V.Vector Char)
+                 -> BS.ByteString
+                -> M.HashMap GeneName (V.Vector Char)
+    addGeneData hm xs
+        | BS.null xs = error "No gene data given"
+        | otherwise = M.insert geneName vectorData hm
+      where
+        geneAndData = BS.break (== delimiter) xs
+        (gName,vData) = geneAndData
+        geneName = GeneName gName
+        vectorData = V.fromList . BS.unpack . BS.filter (/= delimiter) $ vData
+
+
+-- countByMetadata :: (Table -> Metadata) -> M.HashMap GenomeName Table -> M.HashMap Metadata Int
+-- countByMetadata meta = foldl' (addMetadataValue meta) M.empty
+
+
+-- addMetadataValue :: (Table -> a ) -> M.HashMap a Int -> Table -> M.HashMap a Int
+-- addMetadataValue meta hm t = M.insertWith (+) (meta t) 1 hm
+
+
+-- summaryMetadataTable :: M.HashMap a Int -> [String]
+-- summaryMetadataTable hm = sort $ M.foldlWithKey' makeTable [] hm
+--   where
+--     makeTable :: => [String] -> b -> Int -> [String]
+--     makeTable xs name v = (BS.unpack(unMeta name) ++ "\t" ++ show v):xs
+
+
+--This is the general function for returning a HashMap that contains only those
+--entries that matched a particular Metadata entry.
+--getMetadataType returns the record syntax FUNCTION that extracts the particular
+--type of metadata from a table
+filterMetadataInfo :: [Metadata] -> M.HashMap GenomeName Table -> M.HashMap GenomeName Table
+filterMetadataInfo xs = M.filter filterFunc
+  where
+    filterFunc :: Table -> Bool
+    filterFunc t = or allComparisons
+      where
+        allComparisons = fmap currentTest xs
+        currentTest :: Metadata
+                    -> Bool
+        currentTest x = getMetadataType x t == x
+
+
+--this function takes a pre-filtered data structure
+--It returns a list of all column positions for the genomes
+getColumnList :: M.HashMap GenomeName Table
+              -> [Int]
+getColumnList = M.foldl' (\ a v -> (fromJust . unColumnNumber. columnNumber $ v):a ) []
+
+
+--check the character at each index that is passed in
+--returns total count of matches
+countCharInVectorByIndices :: V.Vector Char
+                           -> Char
+                           -> [Int]
+                           -> Int
+countCharInVectorByIndices v matchChar = foldl' aFun 0
+  where
+    aFun :: Int -> Int -> Int
+    aFun rt vi = if matchChar == (v V.! vi)
+          then rt + 1
+          else rt
+
+
+
+
